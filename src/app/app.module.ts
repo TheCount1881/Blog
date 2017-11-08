fa466622bf40ab3b652ff7021e0276bd04c03592
Federico <fedecouti00@gmail.com>
2017-11-08 12:10:22 -0300

    sign-up ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module'
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { SignUpComponent } from './components/auth/sign-up/sign-up.component';
+import { HomeComponent } from './components/home/home.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { PanelComponent } from './components/panel/panel.component';
 
+import { RouteGuard } from './components/auth/route-guard'
+import { NotificationService } from './providers/notification.service';
+import { NotificationComponent } from './components/notification/notification.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    SignUpComponent,
+    HomeComponent,
+    PostsComponent,
+    PanelComponent,
+    NotificationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [
+    RouteGuard,
+    NotificationService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
